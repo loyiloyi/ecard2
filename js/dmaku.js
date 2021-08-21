3bd27e605739a3544ad9fee2fbf0b5adf452d21e
loyiloyi <82072476+loyiloyi@users.noreply.github.com>
2021-08-21 21:42:20 +0800

    Add files via upload

---
+++ js/dmaku.js
@@ -0,0 +1,52 @@
+/****************************************************************
+ *																*		
+ * 						      代码库							*
+ *                        www.dmaku.com							*
+ *       		  努力创建完善、持续更新插件以及模板			*
+ * 																*
+****************************************************************/
+'use strict';
+
+var $circle = document.querySelector('.card__circle');
+var $smallCircle = document.querySelector('.card__smallCircle');
+var $year = document.querySelector('.card__year');
+var $card = document.querySelector('.card');
+var $cardOrangeShine = document.querySelector('.card__orangeShine');
+var $cardThankYou = document.querySelector('.card__thankyou');
+var $cardComet = document.querySelector('.card__cometOuter');
+
+var generateTranslate = function generateTranslate(el, e, value) {
+	el.style.transform = 'translate(' + e.clientX * value + 'px, ' + e.clientY * value + 'px)';
+};
+// http://stackoverflow.com/a/1480137
+var cumulativeOffset = function cumulativeOffset(element) {
+	var top = 0,
+	    left = 0;
+	do {
+		top += element.offsetTop || 0;
+		left += element.offsetLeft || 0;
+		element = element.offsetParent;
+	} while (element);
+
+	return {
+		top: top,
+		left: left
+	};
+};
+document.onmousemove = function (event) {
+	console.log(cumulativeOffset($card));
+	var e = event || window.event;
+	var x = (e.pageX - cumulativeOffset($card).left - 350 / 2) * -1 / 100;
+	var y = (e.pageY - cumulativeOffset($card).top - 350 / 2) * -1 / 100;
+
+	var matrix = [[1, 0, 0, -x * 0.00005], [0, 1, 0, -y * 0.00005], [0, 0, 1, 1], [0, 0, 0, 1]];
+
+	generateTranslate($smallCircle, e, 0.03);
+	generateTranslate($cardThankYou, e, 0.03);
+	generateTranslate($cardOrangeShine, e, 0.09);
+	generateTranslate($circle, e, 0.05);
+	generateTranslate($year, e, 0.03);
+	generateTranslate($cardComet, e, 0.05);
+
+	$card.style.transform = 'matrix3d(' + matrix.toString() + ')';
+};console.log("\u002f\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u000d\u000a\u0020\u002a\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u0009\u0009\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0020\u0020\u0020\u0020\u0020\u0020\u4ee3\u7801\u5e93\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0077\u0077\u0077\u002e\u0064\u006d\u0061\u006b\u0075\u002e\u0063\u006f\u006d\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0009\u0009\u0020\u0020\u52aa\u529b\u521b\u5efa\u5b8c\u5584\u3001\u6301\u7eed\u66f4\u65b0\u63d2\u4ef6\u4ee5\u53ca\u6a21\u677f\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002f");
